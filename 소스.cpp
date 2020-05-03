dd96880a74e359d2cc6d6f3c3953ec43eabb9e2c
yuyunjae03@gmail.com <yuyunjae03@gmail.com>
2020-05-03 16:03:17 +0900

    random move

--- 소스.cpp
+++ 소스.cpp
@@ -1,13 +1,18 @@
+#define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES	1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <bangtal.h>
 
 SceneID scene1, scene2;
 
-ObjectID startButton, endButton, mole1, mole2, mole3, mole4;
-ObjectID santa;
+ObjectID startButton, endButton, explainButton, explain;
+ObjectID moleObjects[10],gameObjects[20];
+int score, num;
+bool flag = false;
 
-int santaX = 0, santaY = 500;
-
-TimerID timer1;
+TimerID timer1, timer2;
 
 
 void startGame()
@@ -15,35 +20,67 @@ void startGame()
 	enterScene(scene2);
 	hideObject(startButton);
 	hideObject(endButton);
+	hideObject(explainButton);
+	char image[100];
+	for (int i = 0; i < 8; i++) {
+		sprintf(image, "두더지-%d.png", i + 1);
+		moleObjects[i] = createObject(image);
+		gameObjects[i] = moleObjects[i]; //gameObjects[7}까지 사용함.
+		locateObject(moleObjects[i], scene2, 100 + 100*i, 100);
+		showObject(moleObjects[i]);
+	}
 
 	timer1 = createTimer(20.f);
 	showTimer(timer1);
 	startTimer(timer1);
+	timer2 = createTimer(0.2f);
+	startTimer(timer2);
 }
 
 
-// 게임 종료를 처리할 함수를 정의한다.
-// 성공인 경우에 success에 true를 전달한다. 실패는 false.
 void endGame(bool success)
 {
 	if (success) {
-		showMessage("선물 배달 성공~~~");
+		showMessage("굿");
 	}
 	else {
-		showMessage("헉, 선물 배달 실패!!!");
+		showMessage("게임 종료~!");
 	}
 
-
-
-	setObjectImage(startButton, "Images/restart.png");
+	setObjectImage(startButton, "restart.png");
+	locateObject(startButton, scene2, 590, 70);
+	locateObject(endButton, scene2, 590, 20);
+	locateObject(explainButton, scene2, 550, 120);
 	showObject(startButton);
 	showObject(endButton);
+	showObject(explainButton);
 
-	santaX = 0;
-	locateObject(santa, scene1, santaX, santaY);
 
 	stopTimer(timer1);
 }
+
+void random_move() {
+	int m, n, o, p;
+	m = rand() % 8;
+	for (int i = 0; i < 8; i++) {
+		if (m == i) {
+			n = rand() % 1180;
+			o = rand() % 620;
+			locateObject(moleObjects[i], scene2, n, o);
+			showObject(moleObjects[i]);
+		}
+	}
+	
+}
+
+int game_index(ObjectID a) {
+	for (int i = 0; i < 16; i++) {
+		if (gameObjects[i] == a) return i;
+	}
+
+	return -1;
+}
+
 
 void mouseCallback(ObjectID object, int x, int y, MouseAction action)
 {
@@ -52,17 +89,29 @@ void mouseCallback(ObjectID object, int x, int y, MouseAction action)
 	}
 	else if (object == startButton) {
 		startGame();
-
 	}
-	
+	else if (object == explainButton) {
+		showObject(explain);
+	}
+//	else if () {
+
+//	}
 }
+
 
 void timerCallback(TimerID timer)
 {
+	random_move();
 	if (timer == timer1) {
 		endGame(false);
 	}
+	else if (timer == timer2) {
+		random_move();
+		timer2 = createTimer(0.2f);
+		startTimer(timer2);	
+	}
 }
+
 
 ObjectID createObject(const char* image, SceneID scene, int x, int y, bool shown)
 {
@@ -76,6 +125,7 @@ ObjectID createObject(const char* image, SceneID scene, int x, int y, bool shown
 	return object;
 }
 
+
 int main()
 {
 	setMouseCallback(mouseCallback);
@@ -85,14 +135,10 @@ int main()
 	scene2 = createScene("두더지 잡기~", "땅.jpg");
 	startButton = createObject("start.png", scene1, 590, 70, true);
 	endButton = createObject("end.png", scene1, 590, 20, true);
-	mole1 = createObject("end.png", scene1, 590, 20, true);
-	endButton = createObject("end.png", scene1, 590, 20, true);
-	endButton = createObject("end.png", scene1, 590, 20, true);
-	endButton = createObject("end.png", scene1, 590, 20, true);
-	endButton = createObject("end.png", scene1, 590, 20, true);
-
+	explainButton = createObject("설명버튼.png", scene1, 550, 120, true);
+	srand((unsigned int)time(NULL));
 	//santa = createObject("Images/santa.png", scene1, santaX, santaY, true);
 	//playButton = createObject("Images/play.png", scene1, 610, 30, false);
-
+	//showMessage("점수는 %d입니다", score);
 	startGame(scene1);
 }
